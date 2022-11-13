176d4f896743c70d5400ac7a6ab9b5d9e89f4a7d
glassofcoke <contatocostamarcos@gmail.com>
2022-11-13 19:15:26 -0300

    adicionando soma e subtração

---
+++ AvaliacaoFinalCalculadora/src/Calculadora.java
@@ -0,0 +1,9 @@
+public class Calculadora {
+    public double adiçao(double numA, double numB) {
+        return numA + numB;
+    }
+
+    public double subtraçao(double numA, double numB) {
+        return numA - numB;
+    }
+}
